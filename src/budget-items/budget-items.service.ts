5cc1d34870f5b6c03507925efec335acef6eda5f
Kiley Poole <kpoole007@gmail.com>
2021-12-29 10:06:23 -0800

    Injected BudgetItemRepository dependency into the BudgetItemService class.

--- src/budget-items/budget-items.service.ts
+++ src/budget-items/budget-items.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateBudgetItemDto } from './dto/create-budget-item.dto';
 import { UpdateBudgetItemDto } from './dto/update-budget-item.dto';
+import { BudgetItem } from './entities/budget-item.entity';
 
 @Injectable()
 export class BudgetItemsService {
+  constructor(
+    @InjectRepository(BudgetItem)
+    private budgetItemRepository: Repository<BudgetItem>
+  ){}
+
+
   create(createBudgetItemDto: CreateBudgetItemDto) {
     return 'This action adds a new budgetItem';
   }
